f4afa86604433ca6600b4cebdadef0d2cc55865a
Diogo Kollross <diogoko@gmail.com>
2021-03-12 12:19:15 -0300

    Fix mix.exs

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Hasher.MixProject do
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      escript: [main_module: Hasher]
+      escript: [main_module: HasherMain]
     ]
   end
 
