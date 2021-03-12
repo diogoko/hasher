39700bdac9e9ed324a39136df5f24c13ec07d409
Diogo Kollross <diogoko@gmail.com>
2021-03-12 12:19:03 -0300

    Split modules

--- lib/hasher.ex
+++ lib/hasher.ex
@@ -1,42 +1,4 @@
 defmodule Hasher do
-  @megabyte 1024 * 1024
-
-  def main(args) do
-    options = [
-      strict: [
-        algo: :string,
-        mode: :string,
-        buffer: :integer,
-        quiet: :boolean,
-      ]
-    ]
-
-    {opts, args, _} = OptionParser.parse(args, options)
-
-    algo = opts[:algo] || "sha256"
-    mode = opts[:mode] || "streaming"
-    buffer_size = opts[:buffer] || 2 * @megabyte
-    quiet = opts[:quiet] || false
-
-    do_hash =
-      if mode == "streaming" do
-        &hash_streaming(&1, &2, buffer_size)
-      else
-        &hash_memory/2
-      end
-
-    print_result =
-      fn {file, hash} ->
-        if not quiet do
-          IO.puts("#{hash}  #{file}")
-        end
-      end
-
-    args
-    |> Enum.map(&do_hash.(&1, String.to_atom(algo)))
-    |> Enum.map(print_result)
-  end
-
   def hash_memory(file, algo) do
     data = File.read!(file)
 
---
+++ lib/hasher_main.ex
@@ -0,0 +1,39 @@
+defmodule HasherMain do
+  @megabyte 1024 * 1024
+
+  def main(args) do
+    options = [
+      strict: [
+        algo: :string,
+        mode: :string,
+        buffer: :integer,
+        quiet: :boolean,
+      ]
+    ]
+
+    {opts, args, _} = OptionParser.parse(args, options)
+
+    algo = opts[:algo] || "sha256"
+    mode = opts[:mode] || "streaming"
+    buffer_size = opts[:buffer] || 2 * @megabyte
+    quiet = opts[:quiet] || false
+
+    do_hash =
+      if mode == "streaming" do
+        &Hasher.hash_streaming(&1, &2, buffer_size)
+      else
+        &Hasher.hash_memory/2
+      end
+
+    print_result =
+      fn {file, hash} ->
+        if not quiet do
+          IO.puts("#{hash}  #{file}")
+        end
+      end
+
+    args
+    |> Enum.map(&do_hash.(&1, String.to_atom(algo)))
+    |> Enum.map(print_result)
+  end
+end
